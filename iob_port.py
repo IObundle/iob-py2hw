453e601114435d99bd6aeaa4dc2d04edb1dbdfb7
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-24 16:42:34 +0100

    Changed `iob_wire` to `super()` in the constructor as sugested

--- iob_port.py
+++ iob_port.py
@@ -4,6 +4,6 @@ class iob_port(iob_wire):
     """Class to represent a port in an iob module"""
         
         def __init__(self, name="", bit_width=1, direction="input"):
-            iob_wire.__init__(self, name, bit_width)
+            super().__init__(self, name, bit_width)
             self.direction = direction
             self.connected_to = None
