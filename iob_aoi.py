527db060b91108798d8614071fc94211aa99c878
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-24 15:36:26 +0100

    Changed all list in `iob_aoi` to dictionaries to facilitate the access to its components

--- iob_aoi.py
+++ iob_aoi.py
@@ -16,43 +16,41 @@ class iob_aoi(iob_module):
         self._connect_instances()
 
     def _create_ports(self):
-        self.port_list = 
-            [
-                a_i = iob_port("a_i", 1, "input"),
-                b_i = iob_port("b_i", 1, "input"),
-                c_i = iob_port("c_i", 1, "input"),
-                d_i = iob_port("d_i", 1, "input"),
-                y_o = iob_port("y_o", 1, "output")
-            ]
+        self.port_list = {}
+        self.port_list["a_i"] = iob_port("a_i", 1, "input")
+        self.port_list["b_i"] = iob_port("b_i", 1, "input")
+        self.port_list["c_i"] = iob_port("c_i", 1, "input")
+        self.port_list["d_i"] = iob_port("d_i", 1, "input")
+        self.port_list["y_o"] = iob_port("y_o", 1, "output")
 
     def _create_wires(self):
-        self.wire_list = 
-            [
-                aab = iob_wire("aab", 1),
-                cad = iob_wire("cad", 1),
-                or_res = iob_wire("or_res", 1)
-            ]
+        self.wire_list = {}
+        self.wire_list["aab"] = iob_wire("aab", 1)
+        self.wire_list["cad"] = iob_wire("cad", 1)
+        self.wire_list["or_res"] = iob_wire("or_res", 1)
+                
     def _create_instances(self):
-        self.instance_list = 
-            [
-                and1 = iob_and("and1"),
-                and2 = iob_and("and2"),
-                or1 = iob_or("or1"),
-                inv1 = iob_inv("inv1")
-            ]
+        self.instance_list = {}
+        self.instance_list["and1"] = iob_and("and1")
+        self.instance_list["and2"] = iob_and("and2")
+        self.instance_list["or1"] = iob_or("or1")
+        self.instance_list["inv1"] = iob_inv("inv1")
 
     def _connect_instances(self):
-        and1.port_list[0].connected_to = a_i
-        and1.port_list[1].connected_to = b_i
-        and1.port_list[2].connected_to = aab
-        and2.port_list[0].connected_to = c_i
-        and2.port_list[1].connected_to = d_i
-        and2.port_list[2].connected_to = cad
-        or1.port_list[0].connected_to = aab
-        or1.port_list[1].connected_to = cad
-        or1.port_list[2].connected_to = or_res
-        inv1.port_list[0].connected_to = or_res
-        inv1.port_list[1].connected_to = y_o
+        self.instance_list["and1"].port_list["a_i"].connected_to = self.port_list["a_i"]
+        self.instance_list["and1"].port_list["b_i"].connected_to = self.port_list["b_i"]
+        self.instance_list["and1"].port_list["y_o"].connected_to = self.wire_list["aab"]
+
+        self.instance_list["and2"].port_list["a_i"].connected_to = self.port_list["c_i"]
+        self.instance_list["and2"].port_list["b_i"].connected_to = self.port_list["d_i"]
+        self.instance_list["and2"].port_list["y_o"].connected_to = self.wire_list["cad"]
+
+        self.instance_list["or1"].port_list["a_i"].connected_to = self.wire_list["aab"]
+        self.instance_list["or1"].port_list["b_i"].connected_to = self.wire_list["cad"]
+        self.instance_list["or1"].port_list["y_o"].connected_to = self.wire_list["or_res"]
+
+        self.instance_list["inv1"].port_list["a_i"].connected_to = self.wire_list["or_res"]
+        self.instance_list["inv1"].port_list["y_o"].connected_to = self.port_list["y_o"]
 
     def __main__:
         #create build directory
--- iob_port.py
+++ iob_port.py
@@ -1,6 +1,6 @@
 from iob_wire import io_wire
 
-class io_port(iob_wire):
+class iob_port(iob_wire):
     """Class to represent a port in an iob module"""
         
         def __init__(self, name="", bit_width=1, direction="input"):
