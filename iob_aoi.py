2ec4b939eb95a97df1b3d54634573f55ce790a6f
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-25 12:20:00 +0100

    Solved mostly identation errors in `iob_aoi`; solved typo in `iob_port`

--- iob_aoi.py
+++ iob_aoi.py
@@ -6,9 +6,13 @@ from named_list import named_list
 
 from iob_port import iob_port
 from iob_wire import iob_wire
+from iob_and import iob_and
+from iob_or import iob_or
+from iob_inv import iob_inv
 
 class iob_aoi(iob_module):
     name = "iob_aoi"
+    isPrimitive = False
 
     @classmethod
     def init_atributes(cls):
@@ -23,7 +27,7 @@ class iob_aoi(iob_module):
         self.name = name
         #port_map is used to connect external wires to this module
         self.port_map = copy.deepcopy(self.port_list)
-        for port in self.port_map.values():
+        for port in self.port_map:
             port.connected_to = None
         self.wire_list = copy.deepcopy(self.wire_list)
         self.instance_list = copy.deepcopy(self.instance_list)
@@ -31,19 +35,20 @@ class iob_aoi(iob_module):
 
     @classmethod
     def _create_ports(cls):
-        cls.port_list = named_list
-            ([
+        cls.port_list = named_list(
+            [
                 iob_port("a_i", 1, "input"),
                 iob_port("b_i", 1, "input"),
                 iob_port("c_i", 1, "input"),
                 iob_port("d_i", 1, "input"),
                 iob_port("y_o", 1, "output")
-            ])
+            ]
+        )
 
     @classmethod
     def _create_wires(cls):
-        cls.wire_list = named_list
-            ([
+        cls.wire_list = named_list(
+            [
                 iob_wire("a_i", 1),
                 iob_wire("b_i", 1),
                 iob_wire("c_i", 1),
@@ -51,18 +56,20 @@ class iob_aoi(iob_module):
                 iob_wire("aab", 1),
                 iob_wire("cad", 1),
                 iob_wire("or_res", 1),
-                io_wire("y_o", 1)
-            ])
+                iob_wire("y_o", 1)
+            ]
+        )
                 
     @classmethod
     def _create_instances(cls):
-        cls.instance_list = named_list
-            ([
+        cls.instance_list = named_list(
+            [
                 iob_and("and1"),
                 iob_and("and2"),
                 iob_or("or1"),
                 iob_inv("inv1")
-            ])
+            ]
+        )
 
     @classmethod
     def _connect_instances(cls):
@@ -83,9 +90,22 @@ class iob_aoi(iob_module):
 
 if __name__ == "__main__":        
         #create build directory
+        build_dir = "build"
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir)
 
         #call constructor
+        iob_aoi.init_atributes()
+        aoi = iob_aoi("aoi1")
 
+        #print object hierarchy
+        print(aoi.name)
+        for port in aoi.port_map:
+            print(port.name)
+        for wire in aoi.wire_list:
+            print(wire.name)
+        for instance in aoi.instance_list:
+            print(instance.name)
         #write verilog file
         
 
--- iob_port.py
+++ iob_port.py
@@ -1,9 +1,9 @@
-from iob_wire import io_wire
+from iob_wire import iob_wire
 
 class iob_port(iob_wire):
     """Class to represent a port in an iob module"""
         
-        def __init__(self, name="", bit_width=1, direction="input"):
-            super().__init__(self, name, bit_width)
-            self.direction = direction
-            self.connected_to = None
+    def __init__(self, name="", bit_width=1, direction="input"):
+        super().__init__(name, bit_width)
+        self.direction = direction
+        self.connected_to = None
