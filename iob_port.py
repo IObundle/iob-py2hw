785001f48a1a67a5a880eddd67120fde0936f759
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-25 14:55:03 +0100

    iob_port: port isn't connected to a wire by default

--- iob_port.py
+++ iob_port.py
@@ -3,7 +3,10 @@ from iob_wire import iob_wire
 class iob_port():
     """Class to represent a port in an iob module"""
         
-    def __init__(self, name="", bit_width=1, direction="input"):
+    def __init__(self, name="", direction="input"):
         self.name = name
         self.direction = direction
-        self.wire = iob_wire(name, bit_width)
+        self.wire = None
+
+    def connect(self, wire):
+        self.wire = wire
