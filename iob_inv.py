c5fd08004cbe33ed004de66a2352347b20bb356d
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-07-26 10:16:49 +0100

    Added the gates used in `iob_aoi` for testing

---
+++ iob_inv.py
@@ -0,0 +1,20 @@
+import copy
+
+from iob_module import iob_module
+from iob_port import iob_port
+from named_list import named_list
+
+class iob_inv(iob_module):
+    """Class to represent an inv gate in an iob module"""
+    port_list = named_list(
+        [
+            iob_port("a_i", 1, "input"),
+            iob_port("y_o", 1, "output")
+        ]
+    )
+   
+    def __init__(self, name=""):
+        self.name = name
+        self.port_map = copy.deepcopy(self.port_list)
+        for port in self.port_map:
+            port.connected_to = None
