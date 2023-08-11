62dcc57a61cdca1b221a4f5767118b4daac07fe4
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2023-08-11 13:36:31 +0100

    Removed `iob_datum` now `iob_port` is a subclass of `iob_wire`

--- iob_wire.py
+++ iob_wire.py
@@ -1,26 +1,53 @@
-from iob_datum import iob_datum
-
-class iob_wire(iob_datum):
+class iob_wire:
     """Class to represent a wire in an iob module"""
+
+    def __init__(self, name, width):
+        self.name = name
+        self.width = width
+        self.value = 'x' * width
+
+    def set_value(self, value):
+        if isinstance(value, str):
+            if len(value) != self.width:
+                raise ValueError(f'Value {value} is not the correct width {self.width}')
+            for c in value:
+                if c not in '01zx':
+                    raise ValueError(f'Value {value} contains invalid character {c}')
+        elif isinstance(value, int):
+            if value < 0:
+                if value < -(2**(self.width-1)):
+                    raise ValueError(f'Value {value} is out of range for width {self.width}')
+                value = 2**self.width + value
+                # convert to binary and pad with 1s to width
+                value = bin(value)[2:]
+                value = '1' * (self.width - len(value)) + value
+            elif value >= 2**self.width:
+                raise ValueError(f'Value {value} is out of range for width {self.width}')
+            else:
+                value = bin(value)[2:]
+                value = '0' * (self.width - len(value)) + value
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
     def print_wire(self):
         print(f"wire [{self.width}-1:0] {self.name};")
+
+    def __str__(self):
+        return f'{self.name}'
 
 def unit_test():
     """Unit test for iob_wire"""
 
-    # Create a wire
-    wire0 = iob_wire("wire0", 8, 1)
-    # Check if the wire is correct
+    wire0 = iob_wire(name="wire0", width=8)
+    wire0.set_value(value=17)
+    
     assert wire0.name == "wire0"
     assert wire0.width == 8
-    assert wire0.value == 1
+    assert wire0.value == '00010001'
     
-    # print the wire
     wire0.print_wire()
-
-    # Print end of unit test
-    print("End of unit test for iob_wire.")
-
             
 # Test the class
 if __name__ == "__main__":
